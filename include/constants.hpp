d403d6f75ae6cd80801ab92d1c6cade437344b29
alex <thissideup@riseup.net>
2015-03-03 12:32:26 +0100

    moved s_catch_signals() to main, it's only ever used there

--- include/constants.hpp
+++ include/constants.hpp
@@ -36,24 +36,6 @@ enum SB_SUB_TYPE {
 static bool SB_MSG_DEBUG = false;
 
 
-static int s_interrupted = 0;
-static void s_signal_handler (int signal_value)
-{
-    s_interrupted = 1;
-}
-
-static void s_catch_signals ()
-{
-#if (!defined(WIN32))
-    struct sigaction action;
-    action.sa_handler = s_signal_handler;
-    action.sa_flags = 0;
-    sigemptyset (&action.sa_mask);
-    sigaction (SIGINT, &action, NULL);
-    sigaction (SIGTERM, &action, NULL);
-#endif
-}
-
 static void s_recv(zmq::socket_t &socket, zmq::socket_t &broadcast, std::stringstream &sstream)
 {
   zmq::message_t z_msg;
--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,24 @@
 #include "boxoffice.hpp"
 #include "publisher.hpp"
 
+static int s_interrupted = 0;
+static void s_signal_handler (int signal_value)
+{
+    s_interrupted = 1;
+}
+
+static void s_catch_signals ()
+{
+#if (!defined(WIN32))
+    struct sigaction action;
+    action.sa_handler = s_signal_handler;
+    action.sa_flags = 0;
+    sigemptyset (&action.sa_mask);
+    sigaction (SIGINT, &action, NULL);
+    sigaction (SIGTERM, &action, NULL);
+#endif
+}
+
 void *boxoffice_thread(void *arg)
 {
   Boxoffice* bo;
